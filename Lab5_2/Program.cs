4ed4a3e34f74d0aaea9a6ee66577dc189e72c11c
Artemiy <Artemiy.vlog@gmail.com>
2021-03-01 11:02:54 +0200

    Finished Lab5_2

--- Lab5_2/Program.cs
+++ Lab5_2/Program.cs
@@ -9,54 +9,148 @@ namespace Lab5_2
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            OkCancel w = new OkCancel();
+            Field a = new Field("Введіть ім'я користувача", "Admin");
+            Field b = new Field("Пароль", "*****");
+            Form w = new WindowFields("Введіть ім'я та пароль", a, b);
+            
             DialogResult rc = w.ShowDialog();
-            Console.WriteLine("result is {0}", rc);
+
+            if (rc == DialogResult.OK)
+            {
+                Console.WriteLine("Ви ввели пароль ’{0}’ ’{1}’", a.value, b.value);
+            }
+            else
+                Console.WriteLine("Ви відмовились від вводу");
+
         }
+    }
+     
+    public class Field
+    {
+        public string name;
+        public string def;
+        public string value;
+
+        public Field(string name, string def)
+        {
+            this.name = name;
+            this.def = def;
+            value = string.Empty;
+        }
+    }
+
+    public class WindowFields : OkCancel
+    {
+        Dictionary<TextBox, Field> fields = null;
+        
+        public WindowFields(string Title, Field a, Field b) : base(Title)
+        {
+            fields = new Dictionary<TextBox, Field>(); // создать список пар
+            fields.Add(addFld(1, b), b); // добавили второе поле ввода
+            fields.Add(addFld(0, a), a); // добавили первое поле ввода
+            okButton.Click += _KeyDown;
+        }
+        public TextBox addFld (int n, Field par) // метод добавляет в окно новое поле ввода
+        {
+            int h = 32;
+
+            Panel p = new Panel(); // панель содержит метку и поле ввода
+            p.Name = string.Format("p{0}", n);
+            p.BorderStyle = BorderStyle.FixedSingle;
+            p.Size = new Size(1, h);
+            p.AutoSize = true;
+            p.Dock = DockStyle.Top;
+            p.Padding = new Padding(0, 0, 0, 8);
+            
+            Panel p1 = new Panel(); // панель для высоты
+            p1.BorderStyle = BorderStyle.FixedSingle;
+            p1.Size = new Size(1, h);
+            p1.Dock = DockStyle.Top; /// <--------
+            p1.BackColor = Color.Blue;
+            p.Controls.Add(p1);
+            
+            Label l1;
+            l1 = new Label();
+            l1.Name = string.Format("l{0}", n);
+            l1.Size = new Size(172, h); ;
+            l1.Text = par.name; // создать новую строку
+            l1.Dock = DockStyle.Right;
+            // Console.Error.WriteLine ("new field: ’{0}’/’{1}’/’{2}’"
+            // , par.name, l1.Text, par.value);
+            l1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            p.Controls.Add(l1);
+            
+            TextBox t1;
+            t1 = new TextBox();
+            t1.TabIndex = n;
+            t1.Name = string.Format("t{0}", n); ;
+            t1.Size = new Size(162, h);
+            t1.Text = par.def;
+            t1.Dock = DockStyle.Right;
+            p.Controls.Add(t1);
+            
+            Controls.Add(p); /// готовое поле ввода
+            
+            return t1;
+        }
+        void _KeyDown(object sender, EventArgs e)
+        {
+            TextBox tb;
+            Field f;
+            foreach (KeyValuePair<TextBox, Field> Item in fields)
+            {
+                tb = Item.Key;
+                f = Item.Value;
+                f.value = tb.Text;
+                tb.Text = f.def; /// а теперь восстанавливается значение по умолчанию.
+                Console.Error.WriteLine("keyDown: ’{0}’:’{1}’ ", f.name, f.value);
+            }
+        }
+
     }
 
     public class OkCancel : Form
     {
-        Button okButton;
-        Button cancleButton;
-        int p = 4;
+        protected Button okButton;
+        protected Button cancleButton;
 
-        public OkCancel()
+        public OkCancel(string text)
         {
+            Text = text;
+            int p = 10;
+            Padding = new Padding(p, p, p, p);
+
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MinimizeBox = false;
             MaximizeBox = false;
             ControlBox = false;
             AutoScroll = false;
-            Padding = new Padding(p, p, p, p);
             AutoSize = true;
+            Size = new Size(10, 10);
 
             Panel p0 = new Panel(); // эта панель задает высоту строки
             p0.Size = new Size(1, 32);
-            p0.Dock = DockStyle.Top;
+            p0.BorderStyle = BorderStyle.Fixed3D;
+            p0.BackColor = Color.Green;
             Controls.Add(p0);
 
             CreateOkButton();
 
-            Panel p1 = new Panel(); // эта панель задает расстояние между кнопками
-            p1.Size = new Size(112, 32);
-            p1.Dock = DockStyle.Right;
-            Controls.Add(p1);
+            p0.Size = new Size(112, 32);
+            p0.Dock = DockStyle.Right;
+            p0.BorderStyle = BorderStyle.Fixed3D;
+            p0.BackColor = Color.Red;
+            Controls.Add(p0);
 
             CreateCancleButton();
 
             DialogResult = DialogResult.Cancel;
             AcceptButton = okButton; // нажатие клавиши Enter как на ок
-            CancelButton = cancleButton; // нажатие клавиши Esc КАК НА Cancel*/
-
-            Controls.Add(okButton);
-            Controls.Add(cancleButton);
+            CancelButton = cancleButton; // нажатие клавиши Esc КАК НА Cancel
+            StartPosition = FormStartPosition.CenterScreen;
         }
 
         public void CreateOkButton()
@@ -66,6 +160,7 @@ namespace Lab5_2
             okButton.Dock = DockStyle.Right; // вправо
             okButton.Text = "Ok";
             okButton.DialogResult = DialogResult.OK;
+            Controls.Add(okButton);
         }
 
         public void CreateCancleButton()
@@ -75,6 +170,7 @@ namespace Lab5_2
             cancleButton.Dock = DockStyle.Right;
             cancleButton.Text = "Cancel";
             cancleButton.DialogResult = DialogResult.Cancel;
+            Controls.Add(cancleButton);
         }
     }
 }
